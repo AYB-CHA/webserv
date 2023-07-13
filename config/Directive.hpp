9de35222d358f18657e1e3cf3884cf4b5a19d41f
Soufiane Amini <samini@e3r8p5.1337.ma>
2023-07-13 16:34:43 +0100

    removed useless cleanup methods and added grammar file

--- config/Directive.hpp
+++ config/Directive.hpp
@@ -6,21 +6,16 @@
 #include <string>
 #include <iostream>
 
-// There's a high chance you might need to refactor this
-// by using polymorphism instead
 class BlockDirective;
 class Directive {
 private:
     std::string name;
     std::vector<std::string> parameters;
     BlockDirective *block;
-
-    void    cleanBlock(BlockDirective *);
 public:
     Directive(const Directive& o);
     Directive(std::string, std::vector<std::string>, BlockDirective*);
     void    debug();
-    void    cleanup();
     ~Directive();
 };
 
