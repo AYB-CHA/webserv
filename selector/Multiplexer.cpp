05a9044d8b6234b066153e581be672625bcda5ad
Soufiane Amini <samini@e3r1p13.1337.ma>
2023-07-17 15:24:24 +0100

    progress on multiplexer

---
+++ selector/Multiplexer.cpp
@@ -0,0 +1,45 @@
+#include "Multiplexer.hpp"
+#include <algorithm>
+#include <cstddef>
+#include <stdexcept>
+#include <sys/errno.h>
+#include <sys/socket.h>
+
+Multiplexer::Multiplexer(std::vector<Server> servers) : mediator(servers) {}
+
+void Multiplexer::run() {
+    for (;;) {
+        mediator.getBatch(ready_servers, read_clients, write_clients);
+
+        for (SIter it = ready_servers.begin(); it != ready_servers.end(); ++it) {
+            int fd = accept(it->getSocketFd(), NULL, NULL);
+            if (fd == -1) {
+                if (errno != EWOULDBLOCK) {
+                    throw std::runtime_error("accept() failed");
+                }
+                break;
+            }
+            mediator.addClient(fd);
+        }
+
+        for (CIter it = write_clients.begin(); it != write_clients.end(); ++it) {
+            // bool bufferisEmpty = it->writeChunk();
+            // if (!bufferisEmpty) {
+            //     CIter client = std::find(it->getSocketFd(), read_clients.begin(), read_clients.end());
+            //     if (client != read_clients.end())
+            //         read_clients.erase(client);
+            // }
+        }
+
+        for (CIter it = read_clients.begin(); it != read_clients.end(); ++it) {
+            // std::string buffer = it->read();
+            // HttpRequest request;
+            // try {
+            //     HttpRequestParser parser(request, buffer);
+            // } catch () {
+            // 
+            // }
+        }
+
+    }
+}
