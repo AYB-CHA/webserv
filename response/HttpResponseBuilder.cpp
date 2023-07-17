dd06c626f2e334630b2553cbdab0922dd10d3197
Ayoub Chaaoui <achaaoui@e3r1p15.1337.ma>
2023-07-17 16:33:14 +0100

    switch form using to_string from stl to fromInt from utils::string namespace

--- response/HttpResponseBuilder.cpp
+++ response/HttpResponseBuilder.cpp
@@ -1,63 +1,65 @@
 #include "HttpResponseBuilder.hpp"
+#include "../utils/string.hpp"
 
 HttpResponseBuilder::HttpResponseBuilder() { this->http_version = "HTTP/1.1"; }
 
 HttpResponseBuilder *HttpResponseBuilder::setStatuscode(int status_code) {
-  this->status_code = status_code;
-  switch (status_code) {
-  case 200:
-    this->status_code_phrase = "OK";
-    break;
-  case 404:
-    this->status_code_phrase = "Not Found";
-    break;
-  case 500:
-    this->status_code_phrase = "Internal Server Error";
-    break;
-  case 405:
-    this->status_code_phrase = "Method Not Allowed";
-    break;
-  case 400:
-    this->status_code_phrase = "Bad Request";
-    break;
-  default:
-    this->status_code_phrase = "Must be implemented!!";
-    break;
-  }
+    this->status_code = status_code;
+    switch (status_code) {
+    case 200:
+        this->status_code_phrase = "OK";
+        break;
+    case 404:
+        this->status_code_phrase = "Not Found";
+        break;
+    case 500:
+        this->status_code_phrase = "Internal Server Error";
+        break;
+    case 405:
+        this->status_code_phrase = "Method Not Allowed";
+        break;
+    case 400:
+        this->status_code_phrase = "Bad Request";
+        break;
+    default:
+        this->status_code_phrase = "Must be implemented!!";
+        break;
+    }
 
-  return this;
+    return this;
 }
 
 HttpResponseBuilder *HttpResponseBuilder::setHeader(const std::string &key,
                                                     const std::string &value) {
-  this->headers[key] = value;
-  return this;
+    this->headers[key] = value;
+    return this;
 }
 
 HttpResponseBuilder *HttpResponseBuilder::pushBody(const std::string &buffer) {
-  this->body += buffer;
-  return this;
+    this->body += buffer;
+    return this;
 }
 
 std::string HttpResponseBuilder::build() {
-  this->setHeader("Content-Length", std::to_string(this->body.length()));
-  std::string response;
-  response += this->http_version + " ";
-  response += std::to_string(this->status_code) + " ";
-  response += this->status_code_phrase + "\r\n";
-  std::map<std::string, std::string>::iterator it;
+    this->setHeader("Content-Length",
+                    utils::string::fromInt(this->body.length()));
+    std::string response;
+    response += this->http_version + " ";
+    response += std::to_string(this->status_code) + " ";
+    response += this->status_code_phrase + "\r\n";
+    std::map<std::string, std::string>::iterator it;
 
-  for (it = this->headers.begin(); it != this->headers.end(); ++it) {
-    response += it->first + ": " + it->second + "\r\n";
-  }
+    for (it = this->headers.begin(); it != this->headers.end(); ++it) {
+        response += it->first + ": " + it->second + "\r\n";
+    }
 
-  response += "\r\n";
-  response += this->body;
-  return (response);
+    response += "\r\n";
+    response += this->body;
+    return (response);
 }
 
 const std::string &HttpResponseBuilder::getStatusPhraseCode() {
-  return this->status_code_phrase;
+    return this->status_code_phrase;
 }
 
 HttpResponseBuilder::~HttpResponseBuilder() {}
--- utils/string.cpp
+++ utils/string.cpp
@@ -1,23 +1,23 @@
 #include "string.hpp"
 
 bool utils::string::hasNoSpace(const std::string &string) {
-  for (size_t i = 0; i < string.length(); i++) {
-    if (string[i] == ' ')
-      return false;
-  }
-  return true;
+    for (size_t i = 0; i < string.length(); i++) {
+        if (string[i] == ' ')
+            return false;
+    }
+    return true;
 }
 
 int utils::string::toInt(const std::string &string) {
-  int integer;
+    int integer;
 
-  std::stringstream ss(string);
-  ss >> integer;
-  return integer;
+    std::stringstream ss(string);
+    ss >> integer;
+    return integer;
 }
 
 std::string utils::string::fromInt(int integer) {
-  std::stringstream ss;
-  ss << integer;
-  return ss.str();
+    std::stringstream ss;
+    ss << integer;
+    return ss.str();
 }
