9e3b9447fb20a14d2692f860b83f4513f20d6164
Soufiane Amini <samini@e3r1p13.1337.ma>
2023-07-17 18:09:31 +0100

    added getSocketFd() and writeChunk()

--- client/Client.cpp
+++ client/Client.cpp
@@ -1,5 +1,22 @@
 #include "Client.hpp"
+#include <cstring>
+#include <stdexcept>
+#include <unistd.h>
 
 Client::Client(const Server& server) : server(server) {}
+
+int Client::getSocketFd() const {
+    return this->socketFd;
+}
+
+bool    Client::writeChunk() {
+    if (writeBuffer.empty())
+        return true;
+    int len = write(socketFd, writeBuffer.c_str(), writeBuffer.length());
+    if (len == -1)
+        throw std::runtime_error(std::string("Client write() error:") + strerror(errno));
+    writeBuffer = writeBuffer.substr(len, writeBuffer.length() - len);
+    return false;
+}
 
 Client::~Client() {}
--- client/Client.hpp
+++ client/Client.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 #include "../server/Server.hpp"
+#include <cerrno>
+#include <string>
 
 class Client {
 private:
+    int     socketFd;
+    std::string writeBuffer;
     const Server& server;
 public:
     Client(const Server& server);
+    bool    writeChunk();
+    int     getSocketFd() const;
     ~Client();
 };
