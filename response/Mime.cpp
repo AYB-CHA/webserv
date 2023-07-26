65420f49f7334ea37706f07f65faa9958426edcc
Ayoub Chaaoui <achaaoui@e3r10p14.1337.ma>
2023-07-26 13:49:37 +0100

    add more mime types

--- response/Mime.cpp
+++ response/Mime.cpp
@@ -5,6 +5,23 @@ Mime *Mime::self = NULL;
 Mime::Mime() {
     // todo: add more...
     this->types["mp4"] = "video/mp4";
+    this->types["html"] = "text/html";
+    this->types["htm"] = "text/html";
+    this->types["css"] = "text/css";
+    this->types["xml"] = "text/xml";
+    this->types["gif"] = "image/gif";
+    this->types["jpeg"] = "image/jpeg";
+    this->types["jpg"] = "image/jpeg";
+    this->types["js"] = "application/javascript";
+    this->types["png"] = "image/png";
+    this->types["svg"] = "image/svg+xml";
+    this->types["webp"] = "image/webp";
+    this->types["ico"] = "image/x-icon";
+    this->types["woff"] = "font/woff";
+    this->types["woff2"] = "font/woff2";
+    this->types["json"] = "application/json";
+    this->types["pdf"] = "application/pdf";
+    this->types["mp3"] = "audio/mpeg";
     this->default_ext = "text/plain";
 }
 
